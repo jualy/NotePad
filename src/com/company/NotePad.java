7e8abe02c5fb2fabacb604bb69205830137807c7
Dmitrijs Fedotovs <dima@fedoto.ws>
2020-12-17 18:29:42 +0200

    NotePad

--- src/com/company/NotePad.java
+++ src/com/company/NotePad.java
@@ -8,7 +8,7 @@ public class NotePad {
     public void createRecord() {
         var rec = new Record();
         rec.setFirstName(InputUtils.askString("First Name"));
-        rec.setFirstName(InputUtils.askString("Last Name"));
+        rec.setLastName(InputUtils.askString("Last Name"));
         rec.setPhone(InputUtils.askString("Phone"));
         rec.setPhone(InputUtils.askString("Email"));
         records.add(rec);
