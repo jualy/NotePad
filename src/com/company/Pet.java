e2365690960fcdb8614d5af81bbd068e3fbc1a07
Dmitrijs Fedotovs <dima@fedoto.ws>
2021-01-04 13:52:21 +0200

    NotePad

--- src/com/company/Pet.java
+++ src/com/company/Pet.java
@@ -33,8 +33,8 @@ public class Pet extends Record {
     public void askData() {
         name = InputUtils.askString("Name");
 
-        for (Species: Species.values()) {
-            System.out.println();
+        for (Species sp: Species.values()) {
+            System.out.println(sp);
         }
         species = InputUtils.askSpecies("Species");
 
--- src/com/company/Species.java
+++ src/com/company/Species.java
@@ -2,35 +2,11 @@ package com.company;
 
 
 public enum Species {
-    CAT {
-        @Override
-        public Record createSpecies() {
-            return new CAT();
-
-        }
-    },
-
-    DOG {
-        @Override
-        public Record createSpecies() {
-            return new DOG();
-        }
-    },
-
-    FISH {
-        @Override
-        public Record createSpecies() {
-            return new FISH();
-        }
-    },
-
-    OTHER {
-        @Override
-        public Record createSpecies() {
-            return new OTHER();
-        }
-    };
-
-
-    public abstract Record createSpecies();
+    CAT,
+    DOG,
+    FISH,
+    OTHER
 }
+
+
+
